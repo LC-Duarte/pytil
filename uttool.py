6d3f49b9fefd393a134ff0fcec35c5463cf9d534
Leonardo C. Duarte <leonardochavesduarte@gmail.com>
2021-03-07 19:41:53 -0300

    fixing typo on uttool file header

--- uttool.py
+++ uttool.py
@@ -1,9 +1,9 @@
-""""
+"""
 uttool 
 Youtube download tool
 Rev 0 - march 2021 - leonardochavesduarte@gmail.com
 For more information use the --help option or read ./README.md
-""""
+"""
 
 #utool
 #Standard dependencies
